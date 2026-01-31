c09c85b3fc89a5eeeeb06f4b7ddcfcd5600545c6
Hirak Mondal <hirakmondal2000@gmai.com>
2026-01-31 23:47:56 +0530

    added Command pattern

---
+++ BehaviouralPatters/src/CommandPattern/CommandPattern.java
@@ -0,0 +1,65 @@
+package CommandPattern;
+
+//Command Interface
+interface Command{
+    void execute();
+}
+//Concrete classes for command interface
+class BoldCommand implements Command{
+    private TextEditor textEditor;
+
+    public BoldCommand(TextEditor testEditor) {
+        this.textEditor = testEditor;
+    }
+
+    @Override
+    public void execute() {
+        textEditor.boldText();
+    }
+}
+class ItalicCommand implements Command{
+    private TextEditor textEditor;
+
+    public ItalicCommand(TextEditor testEditor) {
+        this.textEditor = testEditor;
+    }
+
+    @Override
+    public void execute() {
+        textEditor.italicText();
+    }
+}
+
+//Button class
+class Button{
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+     public void click (){
+        command.execute();
+    }
+
+}
+
+class TextEditor{
+    public void boldText(){
+        System.out.println("Text bold preformed");
+    }
+    public void italicText(){
+        System.out.println("Text italic performed");
+    }
+}
+
+public class CommandPattern {
+    public static void main(String[] args) {
+        TextEditor testEditor = new TextEditor();
+        Button button = new Button();
+        button.setCommand(new BoldCommand(testEditor));
+        button.click();
+
+        button.setCommand(new ItalicCommand(testEditor));
+        button.click();
+    }
+}
